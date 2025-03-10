61c896697366c14a565ca7ca7f0a670aabaa8014
Marelu345 <marelu2005@gmail.com>
2025-03-10 11:29:28 -0500

    Hola

--- Hola2.java
+++ Hola2.java
@@ -3,5 +3,6 @@ import javax.swing.*;
 public class Hola2 {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"Hola Mundo");
+        JOptionPane.showMessageDialog(null,"Hola Mundo");
     }
 }
